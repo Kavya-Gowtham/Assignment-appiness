3f8b5c3b6b23e1b71ebbcb2825d956a0e5d80e87
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2021-03-31 19:53:59 +0530

    final code

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".main-slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 2,
+    appendDots: ".slider-dots",
+    dotsClass: "dots",
+  });
+});
